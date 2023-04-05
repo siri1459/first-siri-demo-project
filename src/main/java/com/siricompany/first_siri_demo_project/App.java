11cca099c7418a511cf0e374c1fe99d4d608507c
siri1459 <122383465+siri1459@users.noreply.github.com>
2023-04-05 11:32:03 +0530

    Update App.java

--- src/main/java/com/siricompany/first_siri_demo_project/App.java
+++ src/main/java/com/siricompany/first_siri_demo_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "welocome to the jenkins file" );
+        System.out.println( "welocome to the jenkins file1 and file2" );
     }
 }
